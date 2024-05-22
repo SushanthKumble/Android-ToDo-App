a9a0f74057fd04f75ea915db42b7d473dfe6d280
unknown <sushanthchuchu7@gmail.com>
2024-05-22 16:36:07 +0530

    second commit

--- app/src/main/java/com/example/sqlitepractice/MainActivity.java
+++ app/src/main/java/com/example/sqlitepractice/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //something changed here
+        //haii
     }
 }
