444574acbc45db17cd439cc9811c7791a06f8529
unknown <sushanthchuchu7@gmail.com>
2024-05-22 17:42:14 +0530

    mainactivity

--- app/src/main/java/com/example/sqlitepractice/MainActivity.java
+++ app/src/main/java/com/example/sqlitepractice/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.sqlitepractice;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sqlitepractice.Utils.Databasehelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView mRecyclerview;
+    private FloatingActionButton fab;
+    private Databasehelper myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //something changed here
-        //haii
-        //haii
+
+        mRecyclerview=findViewById(R.id.recyclerview);
+        fab=findViewById(R.id.fab);
+        myDB=new Databasehelper(MainActivity.this);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
